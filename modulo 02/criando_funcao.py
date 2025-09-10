42d8f9290f132f8532b7d3974008532539c5a0ad
AnaC-Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-09-10 10:11:39 -0300

    refazendo criação de funções com exemplos prontos para entendimento

---
+++ modulo 02/criando_funcao.py
@@ -0,0 +1,15 @@
+def diga_oi():
+    print("Oi! Tudo bem?")
+diga_oi()
+
+def saudar(nome):
+    print(f"Olá, {nome}!")
+saudar("Ana")
+saudar("João")
+
+def somar(a, b):
+    return a + b
+total = somar(5, 3)
+print(total)
+print(total * 2)
+
