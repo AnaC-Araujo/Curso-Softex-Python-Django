bbdbe425ff33fb07c59bd07fa9163f3fd3e213dd
AnaC-Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-09-22 11:46:42 -0300

    : Usar um setter para validar dados numéricos e ter métodos que usam a property

--- modulo 02/aula_7/exercicio02.py
+++ modulo 02/aula_7/exercicio02.py
@@ -1,17 +1,25 @@
+
+from math import pi
 class Circulo:
     def __init__(self, raio:int):
-        self.raio = raio
+        self._raio = raio
     
     @property
-    def raio(self):
-        return self.raio
+    def raio(self) -> int:
+        return self._raio
     
     @raio.setter
-    def raio(self, valor_raio: int):
-        if valor_raio > 0:
-            self.raio = valor_raio
+    def raio(self, novo_raio: int) -> None:
+        if novo_raio > 0 and isinstance(novo_raio, int):
+            self._raio = novo_raio
         else:
-            print("Digite um valor positivo.")
+            print("Digite um valor positivo e inteiro.")
 
-    def calcular_area():
-        
+    def calcular_area(self) -> float:
+        area = pi * self._raio ** 2
+        return print(f"{area:.3f}")
+    
+roda = Circulo(2)
+print(roda.raio)
+roda.raio = 6
+roda.calcular_area()
