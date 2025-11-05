47a1c43837581cb028f73bf311daef4013c32c18
Ana Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-11-05 11:36:52 -0300

    Meu primeiro template

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+   # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = {
+       'nome_usuario':'Ana',
+       'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
+    }
+    return render(request, 'home.html', context)
 
 def dona(request):
     return HttpResponse("<h1>Olá, Ana! Tudo bem?!</h1>")
