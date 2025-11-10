e58badfda4f43a1b013fcc10aa0f1550c4e6ba57
AnaC-Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-11-10 11:35:18 -0300

    Primeiro Models (tarefas)

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa
+from .models import Execucao
 
 # Create your views here.
 def home(request):
    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
-    context = {
+   todas_as_tarefas = Tarefa.objects.all()
+   todas_as_execucao = Execucao.objects.all()
+
+   context = {
        'nome_usuario':'Ana',
-       'tecnologias': ['Python', 'Django', 'HTML', 'CSS']
-    }
-    return render(request, 'home.html', context)
+       'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+       'tarefas' : todas_as_tarefas,
+       'execucoes' : todas_as_execucao,
+   }
+   return render(request, 'home.html', context)
 
 def dona(request):
     return HttpResponse("<h1>Olá, Ana! Tudo bem?!</h1>")
