a8402dcd9ab44c5b17b5951dbab770b1b6a48e79
Ana Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-11-12 11:27:37 -0300

    Primeiro Formulário

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import Execucao
+from .forms import TarefaForm
 
 # Create your views here.
 def home(request):
-   # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
-   todas_as_tarefas = Tarefa.objects.all()
-   todas_as_execucao = Execucao.objects.all()
+   
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
 
-   context = {
-       'nome_usuario':'Ana',
-       'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
-       'tarefas' : todas_as_tarefas,
-       'execucoes' : todas_as_execucao,
-   }
-   return render(request, 'home.html', context)
+        if form.is_valid():
+                form.save()
+                return redirect('home')
+    else:
+        form = TarefaForm() 
+
+    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
+    todas_as_tarefas = Tarefa.objects.all()
+    todas_as_execucao = Execucao.objects.all()
+
+    context = {
+        'nome_usuario':'Ana',
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+        'tarefas' : todas_as_tarefas,
+        'execucoes' : todas_as_execucao,
+        'form' : form,
+    }
+    return render(request, 'home.html', context)
 
 def dona(request):
     return HttpResponse("<h1>Olá, Ana! Tudo bem?!</h1>")
