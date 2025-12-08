4556d9b89662e249a990715e19128f475d561f2e
AnaC-Araujo <226937407+AnaC-Araujo@users.noreply.github.com>
2025-12-08 11:42:13 -0300

    modificações e alterações

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Admin\Documents\Curso-Softex-Python-Django\Curso-Softex-Python-Django\.venv"
+set "VIRTUAL_ENV=c:\Users\Admin\Documents\Curso-Softex-Python-Django\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Admin\Documents\Curso-Softex-Python-Django\Curso-Softex-Python-Django\.venv'
+set -gx VIRTUAL_ENV 'c:\Users\Admin\Documents\Curso-Softex-Python-Django\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
