3e5ca4543be268d4058dba9142b1fde19d7944d1
AnaC-Araujo <carolinaana887@gmail.com>
2025-12-09 12:33:23 -0300

    feat: acrescentando estatitisticas na url

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import ListaTarefasAPIView
 from .views import ContagemTarefasAPIView
+from .views import EstatisticasTarefasAPIView
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/user_id=1', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/contagem/', ContagemTarefasAPIView.as_view(), name='contagem-tarefas'),
+    path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas-tarefas'),
 ]
