2b24c31672b7c6be186498c30f11d972ea855e4b
AnaC-Araujo <carolinaana887@gmail.com>
2025-12-12 14:26:47 -0300

    Adicionando opção tarefa duplicadas e concluir todas

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -3,6 +3,7 @@ from .views import ListaTarefasAPIView
 from .views import ContagemTarefasAPIView
 from .views import EstatisticasTarefasAPIView
 from .views import DetalheTarefaAPIView
+from .views import ConcluirTarefaLoteAPIView
 
 app_name = 'core'
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path('tarefas/contagem/', ContagemTarefasAPIView.as_view(), name='contagem-tarefas'),
     path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas-tarefas'),
     path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa'),
+    path('tarefas/<int:pk>/duplicar/', DetalheTarefaAPIView.as_view()),
+    path('tarefas/concluir-todas/', ConcluirTarefaLoteAPIView.as_view()),
 ]
