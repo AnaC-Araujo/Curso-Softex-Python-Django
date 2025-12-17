a9085023cfa4f9a13dbf64f9dd039c824a46eb52
AnaC-Araujo <carolinaana887@gmail.com>
2025-12-17 11:18:22 -0300

    feat/criando url registro e alterando a tarefas

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListaTarefasAPIView
+from .views import TarefaListCreateAPIView
 from .views import ContagemTarefasAPIView
 from .views import EstatisticasTarefasAPIView
 from .views import DetalheTarefaAPIView
@@ -9,19 +9,21 @@ from .views import LogoutView
 from .views import MeView
 from .views import ChangePasswordView
 from .views import UserStatsView
+from .views import TarefaRetrieveUpdateDestroyAPIView
+from .views import RegisterView
 
 app_name = 'core'
 urlpatterns = [
-    path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
-    path('tarefas/user_id=1', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/', TarefaListCreateAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/contagem/', ContagemTarefasAPIView.as_view(), name='contagem-tarefas'),
     path('tarefas/estatisticas/', EstatisticasTarefasAPIView.as_view(), name='estatisticas-tarefas'),
-    path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa'),
+    path('tarefas/<int:pk>/', TarefaRetrieveUpdateDestroyAPIView.as_view(), name='detalhe-tarefa'),
     path('tarefas/<int:pk>/duplicar/', DetalheTarefaAPIView.as_view()),
     path('tarefas/concluir-todas/', ConcluirTarefaLoteAPIView.as_view()),
     path('teste/', MinhaView.as_view(), name= 'teste-autenticacao'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='usuario-autenticado'),
     path('change-password/', ChangePasswordView.as_view(), name='alterar-senha'),
-    path('stats/', UserStatsView.as_view(), name='estatisticas-usuario')
+    path('stats/', UserStatsView.as_view(), name='estatisticas-usuario'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
